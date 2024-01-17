7069b71f45dc9d86956bdef93e480e65c637d057
Md. Mehedi Hasan <52112265+mehedy201@users.noreply.github.com>
2024-01-18 02:35:33 +0600

    Update index.js
    change contact form subject line

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run(){
             const info = await transporter.sendMail({
                 from: req.body.email, // sender address
                 to: process.env.MY_EMAIL, // list of receivers
-                subject:req.body.subject, // Subject line
+                subject:"Request for a Free Quote - RGV Turf", // Subject line
                 text: req.body.message, // plain text body
                 html: `<p><b>You Got New Message from ${req.body.yourName}<b/></p>
                         <h5>Name: ${req.body.yourName}</h5>
